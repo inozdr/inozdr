cb4a75904a1ee358c171a30e19ee128a76d9f739
Иван Ноздрин <vnozdrin@bk.ru>
2020-09-04 14:35:42 +0300

    pr1

---
+++ pr12/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace nozdrin1
+{
+  class person
+  {
+    static void Main(string[] args)
+    {
+      Person p = new Person("Tony", "Allen", 32, Person.Genders.Male);
+      Console.WriteLine(p.ToString());
+    }
+    class Person
+    {
+      public enum Genders : int { Male, Female };
+      public string firstName;
+      public string lastName;
+      public int age;
+      public Genders gender;
+      public Person(string _firstName, string _lastName, int _age, Genders _gender)
+      {
+        firstName = _firstName; lastName =
+        _lastName; age = _age; gender = _gender;
+      }
+      public override string ToString()
+      {
+        return firstName + " " + lastName + " (" + gender + "), age " + age;
+      }
+    }
+  }
+}
